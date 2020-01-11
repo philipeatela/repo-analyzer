1120916ae61068476c63f011e88ff554033a4405
philipeatela <philipeatela@gmail.com>
2020-01-11 01:55:19 -0300

    feat: get all commits list

--- main.py
+++ main.py
@@ -5,11 +5,27 @@ import sys
 import collections
 import utils
 
-repo_path = sys.argv[1]
-print('Running repo-analyzer for ' + repo_path + '...')
+ANALYZED_BRANCH = 'master'
 
-repo_name = utils.getRepoName(repo_path)
+repo_url = sys.argv[1]
+print('Running repo-analyzer for ' + repo_url + '...')
 
-if(not os.path.exists('/' + repo_name)):
-  Repo.clone_from(repo_path, os.getcwd() + '/' + repo_name)
+repo_name = utils.getRepoName(repo_url)
+repo_destination = os.getcwd() + '/' + repo_name
+repo_dir = os.path.isdir(repo_destination)
 
+print(repo_dir)
+print('the repository exists?', repo_dir)
+
+if(repo_dir):
+  repo = Repo(repo_destination)
+else:
+  repo = Repo.clone_from(repo_url, repo_destination)
+
+all_commits = list(repo.iter_commits(ANALYZED_BRANCH))
+
+print(len(all_commits))
+for commit in all_commits:
+  print('Author: ', str(commit.author))
+  print('Author: ', str(commit.author.email))
+  print('Author: ', str(commit.message))
